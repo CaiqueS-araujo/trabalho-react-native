fd806a67892207e1b0420f928683294c7ae6389d
Douglas Costa Gomes de Jesus <douglascostagomesdejesus@gmail.com>
2026-06-17 20:37:21 -0300

    Revert "Primeira versão da página de Jogos"

--- projeto-pokemon-mobile/src/Routes/DrawRouter/index.tsx
+++ projeto-pokemon-mobile/src/Routes/DrawRouter/index.tsx
@@ -9,12 +9,10 @@ const Drawer = createDrawerNavigator();
 export function DrawerNavigation() {
 
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer.Navigator>
+    <GestureHandlerRootView style={{flex: 1}}>
+    <Drawer.Navigator>
         <Drawer.Screen name='Main' component={Main} />
-
-        <Drawer.Screen name='Games' component={Games} options={{ title: 'Jogos Clássicos' }} />
-      </Drawer.Navigator>
+    </Drawer.Navigator>
     </GestureHandlerRootView>
   )
 }
